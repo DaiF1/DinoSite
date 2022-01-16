09d3e8a8e861fbd59d60c809e9139b0c4ab75ab7
DaiF1 <91914037+DaiF1@users.noreply.github.com>
2022-01-16 17:16:21 +0000

    Files sorted

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(__dirname))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/main.html'));
+    res.sendFile(path.join(__dirname + '/html/main.html'));
 });
 
 app.listen(3000, () => {
