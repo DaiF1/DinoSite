dfc41b9572e1e8072c9694414dbc54c264958a30
DaiF <julie.fiadino@gmail.com>
2022-05-11 21:23:31 +0200

    Fix input handling

--- js/main.js
+++ js/main.js
@@ -61,14 +61,38 @@ dino.draw()
 
 const keys = {
     a: {
-        pressed: false
+        pressed: false,
+        changed: false
     },
     d: {
-        pressed: false
+        pressed: false,
+        changed: false
     },
     w: {
-        pressed: false
+        pressed: false,
+        changed: false
     }
+}
+
+function getkeydown(key)
+{
+    down = key.pressed && key.changed
+    if (down)
+        key.changed = false
+    return down
+}
+
+function getkeyup(key)
+{
+    up = !key.pressed && key.changed
+    if (up)
+        key.changed = false
+    return up
+}
+
+function getkey(key)
+{
+    return key.pressed
 }
 
 function mainloop()
@@ -78,17 +102,17 @@ function mainloop()
     c.fillRect(0, 0, canvas.width, canvas.height)
     dino.update()
 
-    if (keys.a.pressed)
+    if (getkeydown(keys.a))
         dino.velocity.x = -5
-    else if (dino.velocity.x < 0)
-        dino.velocity.x = 0
-    
-    if (keys.d.pressed)
-        dino.velocity.x = 5
-    else if (dino.velocity.x > 0)
+    else if (getkeyup(keys.a) && dino.velocity.x < 0)
         dino.velocity.x = 0
 
-    if (keys.w.pressed && dino.onGround)
+    if (getkeydown(keys.d))
+        dino.velocity.x = 5
+    else if (getkeyup(keys.d) && dino.velocity.x > 0)
+        dino.velocity.x = 0
+
+    if (getkey(keys.w) && dino.onGround)
         dino.jump()
 }
 
@@ -98,12 +122,15 @@ window.addEventListener('keydown', (event) => {
     switch (event.key) {
         case 'd':
             keys.d.pressed = true
+            keys.d.changed = true
             break
         case 'a':
             keys.a.pressed = true
+            keys.a.changed = true
             break
         case 'w':
             keys.w.pressed = true
+            keys.w.changed = true
             break
 
     }
@@ -113,12 +140,15 @@ window.addEventListener('keyup', (event) => {
     switch (event.key) {
         case 'd':
             keys.d.pressed = false
+            keys.d.changed = true
             break
         case 'a':
             keys.a.pressed = false
+            keys.a.changed = true
             break
         case 'w':
             keys.w.pressed = false
+            keys.w.changed = true
             break
 
     }
