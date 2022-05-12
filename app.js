b520a8e83e3120df1c705b82a1714ca5b547d9a0
DaiF1 <>
2022-05-12 07:53:23 +0000

    Change app.js html file path

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(__dirname))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/html/main.html'));
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(3000, () => {
