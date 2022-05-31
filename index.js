ee2ecd1bbe0ca70730c6439b95260dfea7799318
DaiF <julie.fiadino@gmail.com>
2022-05-31 10:04:15 +0200

    Scale canvas to screen size

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.width = 1024
-canvas.height = 576
+canvas.width = screen.width / 2
+canvas.height = screen.height / 2
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
